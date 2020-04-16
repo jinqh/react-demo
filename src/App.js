4fa9e9c174867235e0bdd2513a3ba85079c46294
jinqiheng <18655082669@163.com>
2020-04-16 18:14:57 +0800

    添加redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux'
+
+function counter(state=0, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+const store = createStore(counter)
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+store.dispatch({type: 'INCREMENT'})
+store.dispatch({type: 'INCREMENT'})
+store.dispatch({type: 'DECREMENT'})
 
 function App() {
   return (
